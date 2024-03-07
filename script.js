ccb382dc6183ac8334af5ff2885248f9849e8286
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2024-03-07 16:10:15 +0000

    feat(api-network): add api to get network status

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// API CAMERA AUDIO
 function getUserMedia(constraints) {
 	// if Promise-based API is available, use it
 	if (navigator.mediaDevices) {
@@ -43,4 +44,70 @@ function getStream (type) {
 		.catch(function (err) {
 			alert('Error: ' + err);
 		});
+}
+
+// API BATTERY
+if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)) {
+	var target = document.getElementById('target');
+
+	function handleChange(change) {
+		var timeBadge = new Date().toTimeString().split(' ')[0];
+		var newState = document.createElement('p');
+		newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+		target.appendChild(newState);
+	}
+
+	function onChargingChange() {
+		handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
+	}
+	function onChargingTimeChange() {
+		handleChange('Battery charging time changed to ' + this.chargingTime + ' s');
+	}
+	function onDischargingTimeChange() {
+		handleChange('Battery discharging time changed to ' + this.dischargingTime + ' s');
+	}
+	function onLevelChange() {
+		handleChange('Battery level changed to ' + this.level + '');
+	}
+
+	var batteryPromise;
+
+	if ('getBattery' in navigator) {
+		batteryPromise = navigator.getBattery();
+	} else {
+		batteryPromise = Promise.resolve(navigator.battery);
+	}
+
+	batteryPromise.then(function (battery) {
+		document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
+		document.getElementById('chargingTime').innerHTML = battery.chargingTime + ' s';
+		document.getElementById('dischargingTime').innerHTML = battery.dischargingTime + ' s';
+		document.getElementById('level').innerHTML = battery.level;
+
+		battery.addEventListener('chargingchange', onChargingChange);
+		battery.addEventListener('chargingtimechange', onChargingTimeChange);
+		battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
+		battery.addEventListener('levelchange', onLevelChange);
+	});
+}
+
+// API NETWORK
+
+function getConnection() {
+	return navigator.connection || navigator.mozConnection ||
+		navigator.webkitConnection || navigator.msConnection;
+}
+
+function updateNetworkInfo(info) {
+	document.getElementById('networkType').innerHTML = info.type;
+	document.getElementById('effectiveNetworkType').innerHTML = info.effectiveType;
+	document.getElementById('downlinkMax').innerHTML = info.downlinkMax;
+}
+
+var info = getConnection();
+if (info) {
+	info.onchange = function (event) {
+		updateNetworkInfo(event.target);
+	}
+	updateNetworkInfo(info);
 }
